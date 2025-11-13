85a5ac379852328161e7ad436a1ba83483f93797
XP <smurfs@smurfs.verymad.net>
2025-11-12 18:56:55 -0800

    Make deserialization more permissive

--- src/main/java/gg/xp/xivapi/clienttypes/GameVersion.java
+++ src/main/java/gg/xp/xivapi/clienttypes/GameVersion.java
@@ -1,8 +1,10 @@
 package gg.xp.xivapi.clienttypes;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GameVersion(@JsonProperty List<String> names, @JsonProperty String key) {
 }
