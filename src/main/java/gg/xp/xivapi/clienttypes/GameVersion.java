587464e7ec7f24d0b583d15148d968aff5ee227d
XP <smurfs@smurfs.verymad.net>
2025-12-09 16:44:33 -0800

    Make GameVersion serializable

--- src/main/java/gg/xp/xivapi/clienttypes/GameVersion.java
+++ src/main/java/gg/xp/xivapi/clienttypes/GameVersion.java
@@ -3,8 +3,12 @@ package gg.xp.xivapi.clienttypes;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GameVersion(@JsonProperty List<String> names, @JsonProperty String key) {
+public record GameVersion(@JsonProperty List<String> names, @JsonProperty String key) implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
 }
--- src/main/java/gg/xp/xivapi/clienttypes/XivApiAsset.java
+++ src/main/java/gg/xp/xivapi/clienttypes/XivApiAsset.java
@@ -2,9 +2,10 @@ package gg.xp.xivapi.clienttypes;
 
 import gg.xp.xivapi.assets.AssetFormat;
 
+import java.io.Serializable;
 import java.net.URI;
 
-public interface XivApiAsset<X extends AssetFormat> {
+public interface XivApiAsset<X extends AssetFormat> extends Serializable {
 	URI getURI(String formatRaw);
 	URI getURI(X format);
 }
